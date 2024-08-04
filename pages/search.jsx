3ede5033f00832989828afbeabace7f2c530678c
Jenny Rogers <jrogers1@ufl.edu>
2024-08-04 11:41:08 -0400

    change loading status

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,10 +1,10 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { useState } from "react"
-import Link from "next/link"
-import { withIronSessionSsr } from "iron-session/next"
-import sessionOptions from "../config/session"
-import styles from "../styles/search.module.css"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useState } from "react";
+import Link from "next/link";
+import { withIronSessionSsr } from "iron-session/next";
+import sessionOptions from "../config/session";
+import styles from "../styles/search.module.css";
 
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({ req }) {
@@ -26,16 +26,19 @@ export default function Search(props) {
   const [query, setQuery] = useState("");
   const [bookInfo, setBookInfo] = useState([]);
   const [searchPerformed, setSearchPerformed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
     if (!query.trim()) return;
     setSearchPerformed(true);
+    setLoading(true);
     try {
       const res = await fetch(
         `https://www.googleapis.com/books/v1/volumes?q=${query}`
       );
       const bookData = await res.json();
+      setLoading(false);
       if (bookData.totalItems > 0) {
         setBookInfo(bookData.items);
         setQuery("");
@@ -44,6 +47,7 @@ export default function Search(props) {
         setQuery("");
       }
     } catch (error) {
+      setLoading(false);
       console.error("Error fetching data:", error);
     }
   }
@@ -62,7 +66,7 @@ export default function Search(props) {
                 onChange={(e) => setQuery(e.target.value)}
                 type="text"
                 name="book-search"
-                className={styles.input} 
+                className={styles.input}
               />
               <br />
               <br />
@@ -70,7 +74,8 @@ export default function Search(props) {
             </form>
           </div>
           <div className={styles.resultsContainer}>
-            {searchPerformed && bookInfo.length === 0 && <p>No books found.</p>}
+            {loading && <p>Hang tight...</p>}
+            {!loading && searchPerformed && bookInfo.length === 0 && <p>No books found.</p>}
             {bookInfo.length > 0 && bookInfo.map((book) => (
               <div key={book.id} className={styles.bookItem}>
                 <h3>{book.volumeInfo.title}</h3>
